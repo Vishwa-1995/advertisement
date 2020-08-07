d11ba4d6623eea5cc81c06af128bd7a49ff9da8f
Vishwa Mahanama Muthukumarana <vishmahmuthu@gmail.com>
2020-08-07 17:02:20 +0530

    secondt commit

--- advertisement/src/main/java/com/example/advertisement/Banner.java
+++ advertisement/src/main/java/com/example/advertisement/Banner.java
@@ -2,7 +2,7 @@ package com.example.advertisement;
 
 public class Banner {
 
-    public String showBanner(String text){
+    public static String showBanner(String text){
 
         return text;
     }
